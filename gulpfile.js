85a4b3a997bda63622d5dc6f02a167dd075ddcf5
Edgar Hernandez Garcia <edhernandezga@elektra.com.mx>
2017-05-23 19:22:14 -0500

    Correccion de servidor y watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var pngcrush = require('imagemin-pngcrush');
-var webserver = require('gulp-webserver');
 var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
-var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
 var bases = {
     app: 'app/',
@@ -70,71 +68,77 @@ gulp.task('images', function() {
             svgoPlugins: [{removeViewBox: false}],
             use: [pngcrush()]
         }))
-        .pipe(gulp.dest('webapp/images'));
+        .pipe(gulp.dest('webapp/images'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('copy:estilo', function() {
     return gulp.src(paths.styles)
         .pipe(minifyCSS())
         .pipe(concat('estilos.min.css'))
-        .pipe(gulp.dest('webapp/css/'));
+        .pipe(gulp.dest('webapp/css/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('copy:fonts', function() {
     return gulp.src('fonts/*')
-        .pipe(gulp.dest('webapp/fonts'));
+        .pipe(gulp.dest('webapp/fonts'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('copy:vendor', function() {
     return gulp.src(paths.libs)
         .pipe(concat('vendor.min.js'))
-        .pipe(gulp.dest('webapp/vendor'));
+        .pipe(gulp.dest('webapp/vendor'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('copy:index', function() {
     return gulp.src('index.html')
-        .pipe(gulp.dest('webapp/'));
+        .pipe(gulp.dest('webapp/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('copy:html', function() {
     return gulp.src(paths.html)
-        .pipe(gulp.dest('webapp/view/'));
+        .pipe(gulp.dest('webapp/view/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 
 });
 gulp.task('copy:js', function() {
     return gulp.src(paths.scripts)
         .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('webapp/js/'));
-
-});
-
-gulp.task('web_server', function() {
-    gulp.src('webapp')
-        .pipe(webserver({
-            host: 'localhost',
-            port: 6641,
-            livereload: true,
-            open: true,
-            fallback: 'index.html',
+        .pipe(gulp.dest('webapp/js/'))
+        .pipe(browserSync.reload({
+            stream: true
         }));
+
 });
 
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'webapp'
+        }
+    })
+});
 
-gulp.task('watch', function() {
+gulp.task('default', ['browserSync', 'images','copy:estilo','copy:fonts','copy:vendor','copy:index','copy:html','copy:js'], function (){
     gulp.watch('css/*.css', ['copy:estilo']);
     gulp.watch('js/*.js', ['copy:js']);
     gulp.watch('view/*.html', ['copy:html']);
     gulp.watch('*.html',['copy:index']);
+
 });
-
-gulp.task('build', ['clean'], function () {
-    runSequence(['images','copy:estilo','copy:fonts','copy:vendor','copy:index','copy:html','copy:js']);
-});
-
-gulp.task('default', ['build','web_server','watch']);
-
-/*
- gulp.task('default', ['images','copy:estilo','copy:fonts','copy:vendor','copy:index','copy:html','copy:js','watch'],function(){
- runSequence(['web']);
- });
- */
